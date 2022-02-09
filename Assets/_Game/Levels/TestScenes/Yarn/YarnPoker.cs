ad4383d97b841b2ef710c20823a0386e27760479
nick <nicolas.maclean2326@gmail.com>
2022-02-09 16:16:47 -0600

    YarnObjects and MultiDirector
    yarn object commands still need to be connected to YarnSpinner, but will do so later.
    
    YarnObjects abstract animation/interaction of instances of objects to the idea of an object.
    
    multidirector is a wrapper to PlayableDirector that enables multiple timelines to be stored/played using a dictionary like syntax.

---
+++ Assets/_Game/Levels/TestScenes/Yarn/YarnPoker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YarnPoker : MonoBehaviour
+{
+    [SerializeField]
+    string _instanceName = "";
+
+    [SerializeField]
+    string _timelineName = "";
+
+    [SerializeField]
+    YarnObject yarnObj = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Poke();
+        }
+    }
+
+    void Poke()
+    {
+        yarnObj.SetInstance(_instanceName);
+
+        // this won't wait for the animation to finish (like intended),
+        // but yarn spinner will and that's the purpose of the function.
+        StartCoroutine(yarnObj.Animate(_timelineName, "wait"));
+    }
+}
