d1dc02e2a7649cd2af88d2804d026cf01b565219
Basil <basilelhindi@gmail.com>
2022-02-14 13:02:48 -0600

    Create DataManager and SaveData classes to store game information

---
+++ Assets/_Game/Scripts/Mechanics/State/DataManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance = null;
+
+    private string saveFile;
+
+    public SaveData saveData = new SaveData();
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            //savefile = Application.persistentDataPath + "/savedata.json";
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ReadFile()
+    {
+        if(File.Exists(saveFile))
+        {
+            string fileContents = File.ReadAllText(saveFile);
+            JsonUtility.FromJsonOverwrite(fileContents, saveData);
+        }
+    }
+
+    public void WriteFile()
+    {
+        string jsonString = JsonUtility.ToJson(saveData, true);
+        File.WriteAllText(saveFile, jsonString);
+    }
+}
---
+++ Assets/_Game/Scripts/Mechanics/State/SaveData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    [System.Serializable]
+    public class Interaction
+    {
+        public bool interacted = false;
+    }
+
+    [System.Serializable]
+    public class Dialogue
+    {
+
+    }
+
+    [System.Serializable]
+    public struct Settings
+    {
+        public float volume;
+        public int graphics;
+    }
+
+    public List<Interaction> interactions;
+    public List<Dialogue> dialogues;
+
+    public bool[] journalUnlocks;
+}
