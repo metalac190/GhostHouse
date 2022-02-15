c86e490ed743774ac047775e8531ddd9400a8cf3
Sai Mangipudi <thegeekskm101@gmail.com>
2022-02-14 19:53:28 -0600

    Movement and Interaction System Added

---
+++ Assets/_Game/Levels/TestScenes/MovementController/Scripts/InteractableBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractableBase : MonoBehaviour, IInteractable
+{
+    public virtual void OnHoverEnter()
+    {
+        
+    }
+    public virtual void OnHoverExit()
+    {
+        
+    }
+    public virtual void OnLeftClick()
+    {
+        
+    }
+    public virtual void OnRightClick()
+    {
+        
+    }
+}
---
+++ Assets/_Game/Levels/TestScenes/MovementController/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //Variables
+    string _playerName = "testName";
+
+    //Accessor "Getter" Methods
+    public string GetPlayerName()
+    {
+        return _playerName;
+    }
+
+    //Mutator "Setter" Methods
+    public void SetPlayerName(string _newName)
+    {
+        _newName = _playerName;
+    }
+}
