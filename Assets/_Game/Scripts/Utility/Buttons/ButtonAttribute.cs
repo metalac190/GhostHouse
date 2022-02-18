9c8f261d04957a721e4f0d82f7b33c721dd84847
Brandon Coffey <bmc180001@utdallas.edu>
2022-02-17 19:07:16 -0600

    Organize Editor Stuff for Sound System and Button Attribute

---
+++ Assets/_Game/Scripts/Utility/Buttons/ButtonAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Utility.Buttons
+{
+    public enum ButtonMode
+    {
+        Always,
+        NotPlaying,
+        WhilePlaying
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class ButtonAttribute : Attribute
+    {
+        public readonly string name;
+
+        public ButtonAttribute()
+        {
+        }
+
+        public ButtonAttribute(string name) => this.name = name;
+
+        public ButtonMode Mode { get; set; } = ButtonMode.Always;
+
+        public int Spacing { get; set; } = 0;
+    }
+}
