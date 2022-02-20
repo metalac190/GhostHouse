b792234cd86eeef2c01bcf6916307de67672776b
Jessica Macaspac <jeccamac@gmail.com>
2022-02-19 19:33:53 -0600

    Scene Management
    Created a basic  Scene Management system with the gameObject Grandfather Clock as an asset to access the Seasons Panel to navigate to each season

---
+++ Assets/_Game/Levels/SampleLevel/Scripts/SceneSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSelect : MonoBehaviour
+{
+    [SerializeField] GameObject _seasonPanel = null;
+
+    private void Start()
+    {
+        _seasonPanel.SetActive(false);
+    }
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "GrandfatherClock")
+                {
+                    _seasonPanel.SetActive(true);
+                }
+            }
+        }
+    }
+}
