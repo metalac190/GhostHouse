b42296aefff9c66d090ce7fa0ccd4d0dc17853af
Brandon Coffey <bmc180001@utdallas.edu>
2022-02-22 16:50:24 -0600

    Random Delays on ASC

--- Assets/_Game/Scripts/Utility/Audio/Controllers/AudioSourceController.cs
+++ Assets/_Game/Scripts/Utility/Audio/Controllers/AudioSourceController.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using Utility.Audio.Controllers.Base;
 using Utility.Audio.Helper;
 using Utility.RangedFloats;
+using Utility.ReadOnly;
 
 namespace Utility.Audio.Controllers
 {
@@ -11,13 +13,40 @@ namespace Utility.Audio.Controllers
         [SerializeField] private bool _playOnStart = true;
         [SerializeField] private bool _looping = true;
         [SerializeField, MinMaxRange(0, 100)] private RangedFloat _loopDelay = new RangedFloat(0, 0);
+        [SerializeField, ReadOnly] private float _delay;
+
+        private bool _checkLoop;
 
         private void Start() {
             if (_sfx != null && _playOnStart) {
                 SetSourceProperties(_sfx.GetSourceProperties());
-                Source.loop = _looping;
                 Play();
+                if (_looping) {
+                    if (_loopDelay.MaxValue > 0) {
+                        _checkLoop = _looping;
+                    }
+                    else {
+                        Source.loop = true;
+                    }
+                }
             }
+        }
+
+        private void Update() {
+            if (_checkLoop && !Source.isPlaying) {
+                _delay = _loopDelay.GetRandom();
+                StartCoroutine(LoopDelay());
+            }
+        }
+
+        private IEnumerator LoopDelay() {
+            _checkLoop = false;
+            Stop();
+            for (float t = 0; t < _delay; t += Time.deltaTime) {
+                yield return null;
+            }
+            Play();
+            _checkLoop = true;
         }
     }
 }
