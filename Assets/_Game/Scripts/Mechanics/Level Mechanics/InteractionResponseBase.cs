65c820fe4313e02b6818e21c4df51ec67e4033aa
Brandon Coffey <bmc180001@utdallas.edu>
2022-03-08 19:24:04 -0600

    Modify Interaction Response to use Inheritance

---
+++ Assets/_Game/Scripts/Mechanics/Level Mechanics/InteractionResponseBase.cs
@@ -0,0 +1,27 @@
+﻿using Mechanics.Level_Mechanics;
+using UnityEngine;
+
+public abstract class InteractableResponseBase : MonoBehaviour
+{
+    [SerializeField] private Interactable _interactable = null;
+    
+    private void OnEnable()
+    {
+        if (_interactable == null) return;
+        _interactable.Raise(this);
+    }
+
+    private void Start()
+    {
+        if (_interactable == null) return;
+        _interactable.LoadInteraction();
+    }
+
+    private void OnDisable()
+    {
+        if (_interactable == null) return;
+        _interactable.Unraise(this);
+    }
+
+    public abstract void Invoke();
+}
