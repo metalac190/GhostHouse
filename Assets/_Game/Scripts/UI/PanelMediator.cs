7fc2d4be864b2ac3ea0c4c2ced8ab34f433c4361
Carlos <carlosvaleriano45@yahoo.com>
2022-03-19 23:59:14 -0500

    WIP settings UI <--> settings data

---
+++ Assets/_Game/Scripts/UI/PanelMediator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Communication between the Settings UI and Settings singleton
+public class PanelMediator : MonoBehaviour
+{
+
+}
