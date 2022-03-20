bbc5faa457cfbfb16b49146adf335304b5ca90c7
Carlos <carlosvaleriano45@yahoo.com>
2022-03-19 19:30:41 -0500

    Added settings saving and calls to data manager

--- Assets/_Game/Scripts/UI/Settings.cs
+++ Assets/_Game/Scripts/UI/Settings.cs
@@ -2,17 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Settings menus change settings values
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //Interact Button
+    public bool leftClickInteract = true;
+
+    //Camera Movement
+    public bool useWASD = true;
+    public bool useArrowKeys = true;
+    public bool useClickNDrag = false;
+    public int dragSpeed = 75;
+
+    //Audio Settings
+    public int music = 100;
+    public int SFX = 100;
+    public int dialog = 100;
+    public int ambience = 100;
+
+    //Visual Settings
+    public bool isWindowed = false;
+    public int contrast = 75;
+    public int brightness = 75;
+    public bool largeGUIFont = false;
+    public bool largeTextFont = false;
+
+    //0 - Fancy, 1 - Normal, 2 - Dyslexia Friendly
+    [Range(0, 2)]
+    public int textFont = 0;
+
+    //Load all settings when game starts
+    private void Awake()
     {
-        
+        leftClickInteract = DataManager.Instance.settingsLeftClickInteract;
+        useWASD = DataManager.Instance.settingsCameraWASD;
+        useArrowKeys = DataManager.Instance.settingsCameraArrowKeys;
+        useClickNDrag = DataManager.Instance.settingsClickDrag;
+        dragSpeed = DataManager.Instance.settingsSensitivity;
+        music = DataManager.Instance.settingsMusicVolume;
+        SFX = DataManager.Instance.settingsSFXVolume;
+        dialog = DataManager.Instance.settingsDialogueVolume;
+        ambience = DataManager.Instance.settingsAmbienceVolume;
+        isWindowed = DataManager.Instance.settingsWindowMode;
+        contrast = DataManager.Instance.settingsContrast;
+        brightness = DataManager.Instance.settingsBrightness;
+        largeGUIFont = DataManager.Instance.settingsLargeGUI;
+        largeTextFont = DataManager.Instance.settingsLargeText;
+        textFont = DataManager.Instance.settingsTextFont;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveControlSettings()
     {
-        
+        DataManager.Instance.SaveControlSettings(leftClickInteract, useWASD, useArrowKeys, useClickNDrag, dragSpeed);
+    }
+
+    public void SaveAudioSettings()
+    {
+        DataManager.Instance.SaveAudioSettings(music, SFX, dialog, ambience);
+    }
+
+    public void SaveVisualSettings()
+    {
+        DataManager.Instance.SaveVisualSettings(isWindowed, contrast, brightness, largeGUIFont, largeTextFont, textFont);
     }
 }
