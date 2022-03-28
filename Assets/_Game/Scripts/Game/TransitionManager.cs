5561f719b9146570071335fb6efa8b01b2ea9da0
Basil <basilelhindi@gmail.com>
2022-03-28 00:53:18 -0500

    Load game from file successfully working

--- Assets/_Game/Scripts/Game/TransitionManager.cs
+++ Assets/_Game/Scripts/Game/TransitionManager.cs
@@ -115,6 +115,7 @@ namespace Game
 
         private void NextScene() {
             DataManager.Instance.level = _nextScene;
+            DataManager.Instance.WriteFile();
             SceneManager.LoadScene(_nextScene);
         }
     }
