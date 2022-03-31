f18788a28f6b5fd813f5b9a7bfd60c478ac5bf71
Basil <basilelhindi@gmail.com>
2022-03-30 23:57:12 -0500

    Updated documentation and comments on DataManager

--- Assets/_Game/Scripts/Mechanics/State/DataManager.cs
+++ Assets/_Game/Scripts/Mechanics/State/DataManager.cs
@@ -7,8 +7,7 @@ using Utility.Audio.Managers;
 
 public class DataManager : MonoBehaviour
 {
-    // public instance reference to this script
-    public static DataManager Instance = null;
+    public static DataManager Instance = null;  // Singleton instance
 
     // Reference to AudioMixerController to control volume levels
     [SerializeField] AudioMixerController audioMixerController = null;
@@ -22,15 +21,15 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    // string to hold the path to the savefile
-    private string filePath;
+    private string filePath; // save file for saving & loading
 
-    // reference to SaveData object, will hold values to save
+    // reference to SaveData object, will hold values to save to file
     private SaveData saveData = new SaveData();
 
-    // Save Data fields saved in DataManager
-    public string level { get; set; }
-    public int remainingSpiritPoints { get; set; }
+    public string level { get; set; }       // Current level season
+    public int remainingSpiritPoints { get; set; }  // Current points left to spend
+
+    // Points earned towards the various endings
     public int cousinsEndingPoints { get; set; }
     public int sistersEndingPoints { get; set; }
     public int trueEndingPoints { get; set; }
@@ -81,9 +80,9 @@ public class DataManager : MonoBehaviour
         }
     }
 
+    // Set the values to their default values at the start of Spring on a fresh save
     private void SetDefaultValues()
     {
-
         level = "Spring";
         remainingSpiritPoints = 3;
         cousinsEndingPoints = 0;
@@ -106,12 +105,12 @@ public class DataManager : MonoBehaviour
         settingsTextFont = 0;       // placeholder
     }
 
+    // Load the game from file and set up the game
     private void LoadFile()
     {
         ReadFile();
 
         // Set all loaded settings for the player
-
         SetControlSettings();
         SetAudioSettings();
         SetVisualSettings();
@@ -210,6 +209,7 @@ public class DataManager : MonoBehaviour
         File.WriteAllText(filePath, jsonString);
     }
 
+    // Interactables call this on their Start() to initialize themselves in the interactions dictionary
     public void SetDefaultInteraction(string name) {
         if (interactions.ContainsKey(name)) return;
         interactions.Add(name, false);
@@ -236,6 +236,7 @@ public class DataManager : MonoBehaviour
         }
     }
 
+    // Save settings from the control settings menu
     public void SaveControlSettings(bool leftClick, bool useWASD, bool useArrows, bool clickDrag, int sensitivity)
     {
         settingsLeftClickInteract = leftClick;
@@ -247,6 +248,7 @@ public class DataManager : MonoBehaviour
         SetControlSettings();
     }
 
+    // Update the camera controller with the new settings
     private void SetControlSettings()
     {
         // Set Control settings on camera controller
@@ -258,6 +260,7 @@ public class DataManager : MonoBehaviour
         //else if (settingsClickDrag) { CameraController._cameraMode = CameraMode.CLICKDRAG; }
     }
 
+    // Save settings from the audio settings menu
     public void SaveAudioSettings(int musicVol, int sfxVol, int dialogueVol, int ambVol)
     {
         settingsMusicVolume = musicVol;
@@ -268,6 +271,7 @@ public class DataManager : MonoBehaviour
         SetAudioSettings();
     }
 
+    // Update audio mixer controller with audio values
     private void SetAudioSettings()
     {
         // Assuming 0 to 100 instead of 0 to 1
@@ -277,6 +281,7 @@ public class DataManager : MonoBehaviour
         audioMixerController.SetAmbienceVolume(settingsAmbienceVolume * 0.01f);
     }
 
+    // Save settings from the visual settings menu
     public void SaveVisualSettings(bool windowMode, int contrast, int brightness, bool largeGUIFont, bool largeTextFont, int textFont)
     {
         settingsWindowMode = windowMode;
@@ -289,9 +294,10 @@ public class DataManager : MonoBehaviour
         SetVisualSettings();
     }
 
+    // Update visual settings in the font manager and elsewhere
     private void SetVisualSettings()
     {
-        // Set visual settings wherever
+        // Set visual settings wherever eventually
     }
 
     // Dump all data to the console
