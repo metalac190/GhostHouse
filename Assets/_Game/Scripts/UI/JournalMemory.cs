83ea0b250d7ffcff9927d27a60b13bfb6f93c810
Brandon Coffey <bmc180001@utdallas.edu>
2022-04-12 02:57:49 -0500

    Setup endings in Journal

---
+++ Assets/_Game/Scripts/UI/JournalMemory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class JournalMemory : MonoBehaviour
+{
+    [SerializeField] private JournalMemoryType _ending = JournalMemoryType.True;
+    [SerializeField] private GameObject _obj;
+
+    private void OnEnable() {
+        bool unlocked = true;
+        switch (_ending) {
+            case JournalMemoryType.True:
+                //unlocked = DataManager.Instance.trueEndingUnlocked;
+                break;
+            case JournalMemoryType.Bad:
+                //unlocked = DataManager.Instance.badEndingUnlocked;
+                break;
+            case JournalMemoryType.Sister:
+                //unlocked = DataManager.Instance.sisterEndingUnlocked;
+                break;
+            case JournalMemoryType.Cousin:
+                //unlocked = DataManager.Instance.cousinEndingUnlocked;
+                break;
+        }
+        _obj.SetActive(unlocked);
+    }
+}
+
+public enum JournalMemoryType
+{
+    True,
+    Bad,
+    Sister,
+    Cousin
+}
