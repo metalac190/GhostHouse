83fb264feca486773d9584c5a5857f495b785938
Brandon <bmc939@gmail.com>
2022-04-26 14:47:17 -0500

    Balance Test Debugging in Editor only

--- Assets/_Game/Editor/EndCheckEditor.cs
+++ Assets/_Game/Editor/EndCheckEditor.cs
@@ -7,6 +7,7 @@ using UnityEditor;
 [CustomEditor(typeof(EndCheck))]
 public class EndCheckEditor : Editor
 {
+    private List<Interactable> inScene;
     private int count;
     private int cost;
     private int trueEnd;
@@ -17,6 +18,14 @@ public class EndCheckEditor : Editor
     private List<Interactable> cousinInt;
 
     private void OnEnable() {
+        var stories = (StoryInteractable[])Resources.FindObjectsOfTypeAll(typeof(StoryInteractable));
+        inScene = new List<Interactable>();
+        foreach (var story in stories) {
+            if (!EditorUtility.IsPersistent(story.transform.root.gameObject)) {
+                if (story.Interaction != null) inScene.Add(story.Interaction);
+                if (story.AltInteraction != null) inScene.Add(story.AltInteraction);
+            }
+        }
         var interactables = (Interactable[])Resources.FindObjectsOfTypeAll(typeof(Interactable));
         count = 0;
         cost = 0;
@@ -49,6 +58,40 @@ public class EndCheckEditor : Editor
 
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
+        GUILayout.Label("Interactions in Scene: " + inScene.Count);
+        int cost = 0;
+        int truePoints = 0;
+        int sisterPoints = 0;
+        int cousinPoints = 0;
+        foreach (Interactable i in inScene) {
+            if (i.Cost > 0) {
+                cost++;
+            }
+            if (i.TrueEndingPoints > 0) {
+                truePoints++;
+            }
+            if (i.SisterEndPoints > 0) {
+                sisterPoints++;
+            }
+            if (i.CousinEndingPoints > 0) {
+                cousinPoints++;
+            }
+        }
+        GUILayout.Label("Total cost: " + cost);
+        GUILayout.Label("True Points: " + truePoints);
+        GUILayout.Label("Sister Points: " + sisterPoints);
+        GUILayout.Label("Cousin Points: " + cousinPoints);
+        foreach (Interactable i in inScene) {
+            EditorGUILayout.ObjectField(i, typeof(Interactable), true);
+        }
+        
+        GUILayout.Space(25);
+        GUIStyle style = new GUIStyle {
+            wordWrap = true
+        };
+        GUILayout.Label("Warning: To force update this object, in the project search bar, type \'t:Interactable\' and select all of them." +
+                        "Then come back to this object and it should work. No clue why this is needed.", style);
+        GUILayout.Space(10);
         GUILayout.Label("Total Interactions: " + count);
         GUILayout.Label("Interactions with Cost: " + cost);
         GUILayout.Space(10);
@@ -67,11 +110,6 @@ public class EndCheckEditor : Editor
             EditorGUILayout.ObjectField(i, typeof(Interactable), true);
         }
         GUILayout.Space(10);
-        GUIStyle style = new GUIStyle {
-            wordWrap = true
-        };
-        GUILayout.Label("To force update this object, in the project search bar, type \'t:Interactable\' and select all of them." +
-                        "Then come back to this object and it should work. No clue why this is needed.", style);
     }
 }
 #endif
